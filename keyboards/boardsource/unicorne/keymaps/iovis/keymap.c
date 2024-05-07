923bf657db2e90de53dcbb035d4bf44f27757411
David Marchante <davidmarchan@gmail.com>
2024-05-07 02:39:52 -0400

    feat: try combos

--- keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
@@ -7,16 +7,25 @@ enum layer_number {
     _NUMPAD,
 };
 
-// Home Row Mods
-#define HOME_A LCTL_T(KC_A)
-#define HOME_S LALT_T(KC_S)
-#define HOME_D LGUI_T(KC_D)
-#define HOME_F LSFT_T(KC_F)
+/// Combos
+const uint16_t PROGMEM combo_esc[]   = {KC_TAB, KC_Q, COMBO_END};
+const uint16_t PROGMEM combo_enter[] = {KC_SCLN, KC_QUOT, COMBO_END};
 
-// #define HOME_J  RSFT_T(KC_J)
-#define HOME_K RGUI_T(KC_K)
-#define HOME_L LALT_T(KC_L)
-#define HOME_SC RCTL_T(KC_SCLN)
+combo_t key_combos[] = {
+    COMBO(combo_esc, KC_ESC),
+    COMBO(combo_enter, KC_ENT),
+};
+
+/// Complex key definitions
+// Home Row Mods
+#define HM_A LCTL_T(KC_A)
+#define HM_S LALT_T(KC_S)
+#define HM_D LCMD_T(KC_D)
+#define HM_F LSFT_T(KC_F)
+
+#define HM_COMM RCMD_T(KC_COMM)
+#define HM_DOT LALT_T(KC_DOT)
+#define HM_SLSH RCTL_T(KC_SLSH)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -24,45 +33,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+---------+--------+---------+--------.   ,--------+---------+--------+---------+--------+--------.
        KC_TAB , KC_Q   , KC_W    , KC_E   , KC_R    , KC_T   ,     KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , KC_BSPC,
     //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
-       KC_LCTL, HOME_A , HOME_S  , HOME_D , HOME_F  , KC_G   ,     KC_H   , KC_J    , HOME_K , HOME_L  , HOME_SC, KC_QUOT,
+       KC_LCTL, HM_A   , HM_S    , HM_D   , HM_F    , KC_G   ,     KC_H   , KC_J    , KC_K   , KC_L    , KC_SCLN, KC_QUOT,
     //|--------+--------+---------+--------+---------+--------|   |--------+---------+--------+---------+--------+--------|
-       KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, KC_RSFT,
+       KC_LSFT, KC_Z   , KC_X    , KC_C   , KC_V    , KC_B   ,     KC_N   , KC_M    , HM_COMM, HM_DOT  , HM_SLSH, KC_RSFT,
     //`--------+--------+---------+--------+---------+--------/   \--------+---------+--------+---------+--------+--------'
-                                   KC_LALT, KC_ENT  , MO(1)  ,     MO(2)  , KC_SPC  , KC_RGUI
+                                   KC_LALT, MO(1)   , KC_ENT  ,     MO(2)  , KC_SPC  , KC_RGUI
     //                           `+--------+---------+--------'   `--------+---------+--------+'
     ),
 
     [_RAISE] = LAYOUT_split_3x6_3(
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-       _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
+       KC_NUBS, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        _______, KC_PIPE, KC_UNDS, KC_MINS, KC_PLUS, KC_TILD,     KC_GRV , KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_EQL ,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, KC_BSLS, _______,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                                  _______, _______, _______,     OSL(3) , _______, QK_BOOT
+                                  _______, _______, _______,     TT(3)  , _______, _______
     //                          `+--------+--------+--------'   `--------+---------+--------+'
     ),
 
     [_LOWER] = LAYOUT_split_3x6_3(
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-       KC_NUHS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , QK_BOOT,
+       KC_NUBS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        KC_ESC , KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        _______, _______, _______, _______, _______, _______,     _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END , _______,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                                  _______, _______, OSL(3) ,     _______, _______, _______
+                                  _______, TT(3)  , _______,     _______, _______, KC_CAPS
     //                          `+--------+--------+--------'   `--------+--------+--------+'
     ),
 
     [_NUMPAD] = LAYOUT_split_3x6_3(
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-       QK_BOOT, _______, _______, _______, _______, _______,     _______, KC_7   , KC_8   , KC_9   , KC_ASTR, _______,
+       KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, _______, QK_BOOT,     _______, KC_7   , KC_8   , KC_9   , KC_ASTR, _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,     KC_PLUS, KC_4   , KC_5   , KC_6   , KC_EQL , KC_ENT ,
+       KC_VOLD, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,     KC_PLUS, KC_4   , KC_5   , KC_6   , KC_EQL , KC_ENT ,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, _______, _______, _______, _______, _______,     KC_MINS, KC_1   , KC_2   , KC_3   , KC_SLSH, _______,
+       KC_MUTE, _______, _______, _______, _______, _______,     KC_MINS, KC_1   , KC_2   , KC_3   , KC_SLSH, _______,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
                                   _______, _______, _______,     _______, _______, KC_0
     //                          `+--------+--------+--------'   `--------+--------+--------+'
