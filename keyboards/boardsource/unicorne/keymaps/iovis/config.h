1374a07cc70904b2520149feafbecc3a2d10abdd
David Marchante <davidmarchan@gmail.com>
2024-08-18 19:34:49 -0400

    feat: move some layers away from the right thumb

--- keyboards/boardsource/unicorne/keymaps/iovis/config.h
+++ keyboards/boardsource/unicorne/keymaps/iovis/config.h
@@ -3,7 +3,7 @@
 #define CAPS_WORD_TOGGLE_KEY
 #define COMBO_TERM 25
 #define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM 100 // Messes up TAPPING_TOGGLE for some reason
+#define QUICK_TAP_TERM 200 // Messes up TAPPING_TOGGLE for some reason
 #define TAPPING_TOGGLE 2
 
 // VIA
