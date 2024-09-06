5712fcd64e0ecfed87cc1e4baba45eb37e9306e6
David Marchante <davidmarchan@gmail.com>
2024-09-06 16:53:47 -0400

    feat: tweak numpad, put underscore in base layer

--- keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
@@ -78,17 +78,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        KC_LSFT, HM_Z   , KC_X   , KC_C   , SY_V   , KC_B   ,     KC_N   , SY_M   , KC_COMM, KC_DOT , NV_SLSH, HM_RSFT,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                                  TT(_NU), KC_LALT, MY_MEH ,     CW_TOGG, KC_SPC , TT(_NV)
+                                  TT(_NU), KC_LALT, MY_MEH ,     KC_UNDS, KC_SPC , CW_TOGG
     //                          `+--------+--------+--------'   `--------+--------+--------+'
     ),
 
     [_SY] = LAYOUT_split_3x6_3(
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-       KC_ESC , KC_HASH, KC_LCBR, KC_RCBR, KC_DLR , KC_AT  ,     KC_CIRC, KC_AMPR, KC_LBRC, KC_RBRC, KC_PIPE, KC_BSLS,
+       KC_ESC , XXXXXXX, KC_LABK, KC_RABK, KC_DLR , KC_AT  ,     KC_CIRC, KC_AMPR, KC_LBRC, KC_RBRC, KC_PIPE, KC_BSLS,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_PERC,     KC_GRV , KC_ASTR, KC_LPRN, KC_RPRN, KC_COLN, KC_DQUO,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, KC_LCTL, MY_CSTB, MY_CTAB, M_DASH , MY_EURO,     KC_TILD, KC_UNDS, KC_LABK, KC_RABK, KC_SLSH, KC_ENT ,
+       _______, KC_LCTL, MY_CSTB, MY_CTAB, M_DASH , MY_EURO,     KC_TILD, KC_HASH, KC_LCBR, KC_RCBR, KC_SLSH, KC_ENT ,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
                                   _______, _______, KC_SPC ,     _______, _______, _______
     //                          `+--------+--------+--------'   `--------+--------+--------+'
@@ -108,13 +108,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NU] = LAYOUT_split_3x6_3(
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-       _______, KC_DLR , KC_TILD, KC_LABK, KC_RABK, XXXXXXX,     KC_ASTR, KC_7   , KC_8   , KC_9   , KC_PLUS, _______,
+       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_ASTR, KC_7   , KC_8   , KC_9   , KC_PLUS, _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, _______,     KC_MINS, KC_4   , KC_5   , KC_6   , KC_COLN, KC_EQL ,
+       _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,     KC_MINS, KC_4   , KC_5   , KC_6   , KC_COLN, KC_EQL ,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, KC_LCTL, KC_LALT, KC_LGUI, KC_UNDS, XXXXXXX,     KC_HASH, KC_1   , KC_2   , KC_3   , KC_SLSH, KC_PERC,
+       _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, XXXXXXX,     KC_COMM, KC_1   , KC_2   , KC_3   , KC_DOT , KC_PERC,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                                  _______, KC_COMM, MY_MEH ,     KC_SPC , KC_0   , KC_DOT
+                                  _______, _______, _______,     KC_SPC , KC_0   , KC_SLSH
     //                          `+--------+--------+--------'   `--------+--------+--------+'
     ),
 
