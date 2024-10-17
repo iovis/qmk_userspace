4606bee37e3eb30c757e49ecb7b4ce1a538bab96
David Marchante <davidmarchan@gmail.com>
2024-10-17 10:29:08 -0400

    feat(voyager): sync voyager with latest

--- keyboards/zsa/voyager/keymaps/iovis/keymap.c
+++ keyboards/zsa/voyager/keymaps/iovis/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
 #include "version.h"
+#include "features/achordion.h"
 
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
@@ -13,6 +14,21 @@ enum layer_number {
     _NU,     // Number Pad
 };
 
+/// Complex key definitions
+#define MY_CSTB S(C(KC_TAB))
+#define MY_CTAB C(KC_TAB)
+#define MY_EURO S(RALT(KC_2))
+#define MY_MEH LCA(KC_LCMD)
+#define M_DASH S(RALT(KC_MINS))
+
+#define NU_F LT(_NU, KC_F)
+#define SY_M LT(_SY, KC_M)
+#define SY_V LT(_SY, KC_V)
+
+#define TG_LCLR TOGGLE_LAYER_COLOR
+#define RGB_FWD RGB_MODE_FORWARD
+
+/// Macros
 enum custom_keycodes {
     RGB_SLD = ML_SAFE_RANGE,
     SMTD_KEYCODES_BEGIN,
@@ -22,6 +38,7 @@ enum custom_keycodes {
     SMTD_KEYCODES_END,
 };
 
+/// SMTD
 // There's a bug in v0.4 that requires to put it here
 #include "features/sm_td.h"
 void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
@@ -44,7 +61,64 @@ uint32_t get_smtd_timeout(uint16_t keycode, smtd_timeout timeout) {
     return get_smtd_timeout_default(timeout);
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+/// Achordion
+void matrix_scan_user(void) {
+    achordion_task();
+}
+
+bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record, uint16_t other_keycode, keyrecord_t* other_record) {
+    // Allow same hand holds
+    switch (tap_hold_keycode) {
+        case NU_F:
+            switch (other_keycode) {
+                case KC_LALT:
+                case KC_LGUI:
+                case KC_LSFT:
+                case KC_Z:
+                case MY_MEH:
+                    return true;
+            }
+
+            break;
+        case SY_V:
+            switch (other_keycode) {
+                case KC_C:
+                case KC_X:
+                case KC_Z:
+                case MY_MEH:
+                    return true;
+            }
+
+            break;
+    }
+
+    return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
+// How long to leave a key press till achordion ignores it and
+// does a hold (default 1000ms)
+uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+    // Bypass Achordion for these keys.
+    // switch (tap_hold_keycode) {
+    //   case HM_Z:
+    //     return 0;
+    // }
+
+    return TAPPING_TERM * 2;
+}
+
+uint16_t achordion_streak_chord_timeout(uint16_t tap_hold_keycode, uint16_t next_keycode) {
+    // Disable achordion streak for some keys
+    switch (tap_hold_keycode) {
+        case NU_F:
+            return 0;
+    }
+
+    return 100;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (!process_achordion(keycode, record)) return false;
     if (!process_smtd(keycode, record)) return false;
 
     switch (keycode) {
@@ -59,20 +133,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
-/// Complex key definitions
-#define MY_CSTB S(C(KC_TAB))
-#define MY_CTAB C(KC_TAB)
-#define MY_EURO S(RALT(KC_2))
-#define MY_MEH LCA(KC_LCMD)
-#define M_DASH S(RALT(KC_MINS))
-
-#define NU_F LT(_NU, KC_F)
-#define SY_M LT(_SY, KC_M)
-#define SY_V LT(_SY, KC_V)
-
-#define TG_LCLR TOGGLE_LAYER_COLOR
-#define RGB_FWD RGB_MODE_FORWARD
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BA] = LAYOUT_voyager(
@@ -85,9 +145,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SY] = LAYOUT_voyager(
     _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, KC_BSPC,
-    _______, XXXXXXX, KC_LABK, KC_RABK, KC_DLR , KC_AT  ,                       KC_CIRC, KC_AMPR, KC_LBRC, KC_RBRC, KC_PIPE, KC_BSLS,
-    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_PERC,                       KC_GRV , KC_ASTR, KC_LPRN, KC_RPRN, KC_COLN, KC_DQUO,
-    _______, KC_LCTL, MY_CSTB, MY_CTAB, M_DASH , MY_EURO,                       KC_TILD, KC_HASH, KC_LCBR, KC_RCBR, KC_SLSH, KC_ENT ,
+    _______, MY_EURO, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLS, XXXXXXX,
+    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,                       KC_COLN, KC_LBRC, KC_LPRN, KC_LCBR, KC_SCLN, KC_DQUO,
+    _______, KC_QUES, KC_LABK, KC_RABK, KC_GRV , M_DASH ,                       KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_SLSH, KC_ENT ,
                                                  _______, KC_SPC ,     _______, _______
   ),
 
@@ -95,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, TG_LCLR, RGB_FWD, RGB_SLD, XXXXXXX, RGB_TOG,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
     _______, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     _______, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, RGB_VAI,                       KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
-    _______, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX, RGB_VAD,                       KC_END , KC_HOME, KC_PGUP, KC_PGDN, _______, QK_BOOT,
+    _______, KC_LCTL, MY_CSTB, MY_CTAB, XXXXXXX, RGB_VAD,                       KC_END , KC_HOME, KC_PGUP, KC_PGDN, _______, QK_BOOT,
                                                  _______, _______,     QK_LLCK, CW_TOGG // layer lock seems to not work with sm_td
   ),
 
@@ -110,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NU] = LAYOUT_voyager(
     _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                       MY_EURO, KC_DLR , KC_ASTR, KC_PERC, KC_TILD, _______,
     _______, _______, _______, _______, _______, _______,                       KC_SLSH, KC_7   , KC_8   , KC_9   , KC_PLUS, KC_TAB ,
-    _______, _______, _______, KC_LSFT, _______, _______,                       KC_COLN, KC_4   , KC_5   , KC_6   , KC_MINS, KC_EQL ,
+    _______, _______, _______, _______, _______, _______,                       KC_COLN, KC_4   , KC_5   , KC_6   , KC_MINS, KC_EQL ,
     _______, _______, _______, _______, _______, _______,                       KC_COMM, KC_1   , KC_2   , KC_3   , KC_DOT , KC_ENT ,
                                                  _______, QK_LLCK,     KC_SPC , KC_0
   ),
@@ -155,14 +215,14 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     [_SY] = {
         // Left
         BLUE , GREEN, GREEN, GREEN, GREEN, GREEN,
-        BLUE , BLACK, ORANG, ORANG, ORANG, ORANG,
         BLUE , ORANG, ORANG, ORANG, ORANG, ORANG,
-        BLUE , BLUE , BLUE , BLUE , ORANG, ORANG,
+        BLUE , ORANG, ORANG, ORANG, ORANG, ORANG,
+        BLUE , ORANG, ORANG, ORANG, ORANG, ORANG,
                                     MAGNT, CYAN ,
 
         // Right
         GREEN, GREEN, GREEN, GREEN, GREEN, BLUE ,
-        ORANG, ORANG, ORANG, ORANG, ORANG, ORANG,
+        ORANG, ORANG, ORANG, ORANG, ORANG, BLACK,
         ORANG, ORANG, ORANG, ORANG, ORANG, ORANG,
         ORANG, ORANG, ORANG, ORANG, ORANG, BLUE ,
         MAGNT, CYAN ,
@@ -209,10 +269,10 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
                                     ORANG, YELLW,
 
         // Right
-        MAGNT, MAGNT, MAGNT, MAGNT, MAGNT, BLUE ,
-        CYAN , GREEN, GREEN, GREEN, CYAN , CYAN ,
-        CYAN , GREEN, GREEN, GREEN, CYAN , CYAN ,
-        CYAN , GREEN, GREEN, GREEN, CYAN , CYAN ,
+        ORANG, ORANG, ORANG, ORANG, ORANG, BLUE ,
+        ORANG, GREEN, GREEN, GREEN, ORANG, BLUE ,
+        ORANG, GREEN, GREEN, GREEN, ORANG, ORANG,
+        ORANG, GREEN, GREEN, GREEN, ORANG, BLUE ,
         CYAN , GREEN,
     },
 
