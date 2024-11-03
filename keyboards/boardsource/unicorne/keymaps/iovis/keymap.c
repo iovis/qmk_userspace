465ab247a2b94f01ca5862fd49a619dc1c6d558d
David Marchante <davidmarchan@gmail.com>
2024-11-03 17:09:02 -0500

    feat(symbols): put the () and [] back together

--- keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
@@ -222,9 +222,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
        KC_ESC , KC_GRV , KC_AT  , KC_HASH, KC_DLR , KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLS, _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,     KC_COLN, KC_LBRC, KC_LCBR, KC_LPRN, KC_SCLN, KC_DQUO,
+       _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,     KC_COLN, KC_LCBR, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, MY_EURO, KC_LABK, KC_RABK, KC_QUES, M_DASH ,     KC_TILD, KC_RPRN, KC_RBRC, KC_RCBR, KC_SLSH, KC_ENT ,
+       _______, MY_EURO, KC_LABK, KC_RABK, KC_QUES, M_DASH ,     KC_TILD, KC_RCBR, KC_LBRC, KC_RBRC, KC_SLSH, KC_ENT ,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
                                   _______, _______, KC_SPC ,     _______, _______, _______
     //                          `+--------+--------+--------'   `--------+--------+--------+'
