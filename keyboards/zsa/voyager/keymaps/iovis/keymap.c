17bda148e71a5bb275f7d055d0268f4fcb7f91b4
David Marchante <davidmarchan@gmail.com>
2024-11-03 23:22:33 -0500

    feat(voyager): parity with unicorne

--- keyboards/zsa/voyager/keymaps/iovis/keymap.c
+++ keyboards/zsa/voyager/keymaps/iovis/keymap.c
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2024 David Marchante
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include QMK_KEYBOARD_H
 
 #include "version.h"
@@ -7,34 +29,36 @@
 #define ML_SAFE_RANGE SAFE_RANGE
 
 enum layer_number {
-    _BA = 0, // Base
-    _SY,     // Symbols
-    _NV,     // Navigation
-    _GA,     // Gaming
-    _NU,     // Number Pad
+    LAYER_BASE = 0, // Base
+    LAYER_SYM,      // Symbols
+    LAYER_NAV,      // Navigation
+    LAYER_GAME,     // Gaming
+    LAYER_NUM,      // Number Pad
 };
 
 /// Complex key definitions
+// clang-format off
 #define MY_CSTB S(C(KC_TAB))
 #define MY_CTAB C(KC_TAB)
 #define MY_EURO S(RALT(KC_2))
-#define MY_MEH LCA(KC_LCMD)
-#define M_DASH S(RALT(KC_MINS))
+#define MY_MEH  LCA(KC_LCMD)
+#define M_DASH  S(RALT(KC_MINS))
 
 #define HM_RSFT RSFT_T(KC_ENT)
-#define NU_F LT(_NU, KC_F)
-#define SY_M LT(_SY, KC_M)
-#define SY_V LT(_SY, KC_V)
+#define NU_F    LT(LAYER_NUM, KC_F)
+#define NV_SLSH LT(LAYER_NAV, KC_SLSH)
+#define SY_SCLN LT(LAYER_SYM, KC_SCLN)
+#define SY_A    LT(LAYER_SYM, KC_A)
 
 #define TG_LCLR TOGGLE_LAYER_COLOR
 #define RGB_FWD RGB_MODE_FORWARD
+// clang-format on
 
 /// Macros
 enum custom_keycodes {
     RGB_SLD = ML_SAFE_RANGE,
     SMTD_KEYCODES_BEGIN,
     HM_Z,
-    NV_SLSH,
     SMTD_KEYCODES_END,
 };
 
@@ -42,10 +66,7 @@ enum custom_keycodes {
 // There's a bug in v0.4 that requires to put it here
 #include "features/sm_td.h"
 void on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
-    switch (keycode) {
-        SMTD_MT(HM_Z, KC_Z, KC_LCTL)
-        SMTD_LT(NV_SLSH, KC_SLSH, _NV)
-    }
+    switch (keycode) { SMTD_MT(HM_Z, KC_Z, KC_LCTL) }
 }
 
 uint32_t get_smtd_timeout(uint16_t keycode, smtd_timeout timeout) {
@@ -53,7 +74,6 @@ uint32_t get_smtd_timeout(uint16_t keycode, smtd_timeout timeout) {
     if (timeout == SMTD_TIMEOUT_RELEASE) {
         switch (keycode) {
             case HM_Z:
-            case NV_SLSH:
                 return 50;
         }
     }
@@ -80,11 +100,20 @@ bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record, ui
             }
 
             break;
-        case SY_V:
+        case NV_SLSH:
             switch (other_keycode) {
-                case KC_C:
-                case KC_X:
-                case KC_Z:
+                case KC_H:
+                case KC_J:
+                case KC_K:
+                case KC_L:
+                case KC_SPC:
+                case KC_BSPC:
+                    return true;
+            }
+
+            break;
+        case SY_A:
+            switch (other_keycode) {
                 case MY_MEH:
                     return true;
             }
@@ -111,7 +140,7 @@ uint16_t achordion_streak_chord_timeout(uint16_t tap_hold_keycode, uint16_t next
     // Disable achordion streak for some keys
     switch (tap_hold_keycode) {
         case HM_RSFT:
-        case NU_F:
+        case SY_SCLN:
             return 0;
     }
 
@@ -136,39 +165,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BA] = LAYOUT_voyager(
+  [LAYER_BASE] = LAYOUT_voyager(
     KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                       KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
-    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                       KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , TO(_GA),
-    KC_LGUI, KC_A   , KC_S   , KC_D   , NU_F   , KC_G   ,                       KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-    KC_LSFT, HM_Z   , KC_X   , KC_C   , SY_V   , KC_B   ,                       KC_N   , SY_M   , KC_COMM, KC_DOT , NV_SLSH, HM_RSFT,
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                       KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , TO(LAYER_GAME),
+    KC_LGUI, SY_A   , KC_S   , KC_D   , NU_F   , KC_G   ,                       KC_H   , KC_J   , KC_K   , KC_L   , SY_SCLN, KC_QUOT,
+    KC_LSFT, HM_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                       KC_N   , KC_M   , KC_COMM, KC_DOT , NV_SLSH, HM_RSFT,
                                                  KC_LALT, MY_MEH ,     KC_UNDS, KC_SPC
   ),
 
-  [_SY] = LAYOUT_voyager(
+  [LAYER_SYM] = LAYOUT_voyager(
     _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, KC_BSPC,
-    _______, MY_EURO, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLS, XXXXXXX,
-    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,                       KC_COLN, KC_LBRC, KC_LPRN, KC_LCBR, KC_SCLN, KC_DQUO,
-    _______, KC_QUES, KC_LABK, KC_RABK, KC_GRV , M_DASH ,                       KC_TILD, KC_RPRN, KC_RCBR, KC_RBRC, KC_SLSH, KC_ENT ,
+    _______, KC_GRV , KC_AT  , KC_HASH, KC_DLR , KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLS, XXXXXXX,
+    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,                       KC_COLN, KC_LCBR, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO,
+    _______, MY_EURO, KC_LABK, KC_RABK, KC_QUES, M_DASH ,                       KC_TILD, KC_RCBR, KC_LBRC, KC_RBRC, KC_SLSH, KC_ENT ,
                                                  _______, KC_SPC ,     _______, _______
   ),
 
-  [_NV] = LAYOUT_voyager(
+  [LAYER_NAV] = LAYOUT_voyager(
     _______, TG_LCLR, RGB_FWD, RGB_SLD, XXXXXXX, RGB_TOG,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-    _______, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    _______, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_LLCK,
     _______, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, RGB_VAI,                       KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
     _______, KC_LCTL, MY_CSTB, MY_CTAB, XXXXXXX, RGB_VAD,                       KC_END , KC_HOME, KC_PGUP, KC_PGDN, _______, QK_BOOT,
-                                                 _______, _______,     QK_LLCK, CW_TOGG // layer lock seems to not work with sm_td
+                                                 _______, _______,     KC_CAPS, CW_TOGG
   ),
 
-  [_GA] = LAYOUT_voyager(
+  [LAYER_GAME] = LAYOUT_voyager(
     KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                       KC_6   , KC_7   , KC_8   , KC_9  , KC_0   , KC_BSPC,
-    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                       KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , TO(_BA),
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                       KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , TO(0)  ,
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                       KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                       KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_ENT ,
                                                  KC_SPC , KC_LALT,     KC_LGUI, KC_SPC
   ),
 
-  [_NU] = LAYOUT_voyager(
+  [LAYER_NUM] = LAYOUT_voyager(
     _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                       MY_EURO, KC_DLR , KC_ASTR, KC_PERC, KC_TILD, _______,
     _______, _______, _______, _______, _______, _______,                       KC_SLSH, KC_7   , KC_8   , KC_9   , KC_PLUS, KC_TAB ,
     _______, _______, _______, _______, _______, _______,                       KC_COLN, KC_4   , KC_5   , KC_6   , KC_MINS, KC_EQL ,
@@ -197,7 +226,7 @@ void keyboard_post_init_user(void) {
 #define YELLW { HSV_YELLOW }
 
 const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
-    [_BA] = {
+    [LAYER_BASE] = {
         // Left
         BLUE , GREEN, GREEN, GREEN, GREEN, GREEN,
         BLUE , CYAN , CYAN , CYAN , CYAN , CYAN ,
@@ -213,7 +242,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         CYAN , CYAN ,
     },
 
-    [_SY] = {
+    [LAYER_SYM] = {
         // Left
         BLUE , GREEN, GREEN, GREEN, GREEN, GREEN,
         BLUE , ORANG, ORANG, ORANG, ORANG, ORANG,
@@ -229,7 +258,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         MAGNT, CYAN ,
     },
 
-    [_NV] = {
+    [LAYER_NAV] = {
         // Left
         BLUE , RED  , RED  , RED  , BLACK, RED  ,
         BLUE , WHITE, WHITE, WHITE, BLACK, BLACK,
@@ -245,7 +274,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         ORANG, MAGNT,
     },
 
-    [_GA] = {
+    [LAYER_GAME] = {
         // Left
         BLUE , GREEN, GREEN, GREEN, GREEN, GREEN,
         BLUE , CYAN , MAGNT, CYAN , CYAN , CYAN ,
@@ -261,7 +290,7 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
         BLUE , CYAN ,
     },
 
-    [_NU] = {
+    [LAYER_NUM] = {
         // Left
         BLUE , MAGNT, MAGNT, MAGNT, MAGNT, MAGNT,
         BLUE , CYAN , CYAN , CYAN , CYAN , CYAN ,
