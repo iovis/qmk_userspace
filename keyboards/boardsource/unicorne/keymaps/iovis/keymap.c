42a2815a35f14716d0bd00d1c9e1b516fbb4afdd
David Marchante <davidmarchan@gmail.com>
2024-11-11 22:44:18 -0500

    feat(gaming): try moving WASD a column to the right
    Otherwise the keys are a bit awkward?

--- keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
@@ -228,13 +228,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_GAME] = LAYOUT_split_3x6_3(
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-       KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+       KC_ESC , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+       KC_G   , KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   ,     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , NV_SLSH, KC_ENT ,
+       KC_B   , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   ,     KC_N   , KC_M   , KC_COMM, KC_DOT , NV_SLSH, KC_ENT ,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
-                                  KC_LALT, KC_SPC , KC_ESC ,     KC_SPC , TT_NUM , QK_LEAD
+                                  KC_T   , KC_LALT, KC_SPC ,     KC_SPC , TT_NUM , QK_LEAD
     //                          `+--------+--------+--------'   `--------+--------+--------+'
     ),
 
@@ -382,14 +382,14 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
     [LAYER_GAME] = {
         // Left
         BLACK, BLACK, BLACK, BLACK, BLACK, BLACK,
-        BLUE ,
+        CYAN ,
         CYAN , CYAN , CYAN , // BGT
-        CYAN , CYAN , CYAN , // RFV
-        CYAN , MAGNT,
-        CYAN , MAGNT, CYAN , // CDE
-        MAGNT, MAGNT, CYAN , // WSX
-        CYAN , MAGNT, CYAN , // ZAQ
-        BLUE , BLUE , BLUE , // TAB, CMD, SFT
+        CYAN , MAGNT, CYAN , // RFV
+        MAGNT, CYAN ,
+        CYAN , MAGNT, MAGNT, // CDE
+        CYAN , MAGNT, CYAN , // WSX
+        BLUE , BLUE , BLUE , // ZAQ
+        BLUE , CYAN , CYAN , // TAB, CMD, SFT
 
         // Right
         BLACK, BLACK, BLACK, BLACK, BLACK, BLACK,
