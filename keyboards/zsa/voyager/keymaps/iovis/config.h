6c5b89e33158954d6e26fb585ce2484937f4fc25
David Marchante <davidmarchan@gmail.com>
2024-11-17 12:02:02 -0500

    refactor(config.h): extract common config.h

--- keyboards/zsa/voyager/keymaps/iovis/config.h
+++ keyboards/zsa/voyager/keymaps/iovis/config.h
@@ -1,22 +1,12 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
+#pragma once
+
+#include "iovis/config.h"
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define SERIAL_NUMBER "6p4AX/Lw3JX"
 #define LAYER_STATE_8BIT
 
-#define TAPPING_TERM 175
-#define MAX_DEFERRED_EXECUTORS 10
-
-// achordion
-#define ACHORDION_STREAK
-
 // RGB
-#undef RGB_MATRIX_TIMEOUT
-#define RGB_MATRIX_TIMEOUT 300000
-
 #define RGB_MATRIX_STARTUP_SPD 60
 
 #undef ENABLE_RGB_MATRIX_ALPHAS_MODS
