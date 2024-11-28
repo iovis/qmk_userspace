f0273008ca7361ba9705775ffe68fb7e93aaeb64
David Marchante <davidmarchan@gmail.com>
2024-11-27 23:44:00 -0500

    feat(voyager.gaming): move WASD to the center

--- keyboards/zsa/voyager/keymaps/iovis/keymap.c
+++ keyboards/zsa/voyager/keymaps/iovis/keymap.c
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LAYER_GAME] = LAYOUT_voyager(
-    KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                       KC_6   , KC_7   , KC_8   , KC_9  , KC_0   , KC_BSPC,
-    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                       KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , QK_LEAD,
-    KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                       KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                       KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_ENT ,
-                                                 KC_SPC , KC_LALT,     KC_LGUI, KC_SPC
+    KC_5   , KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   ,                       KC_6   , KC_7   , KC_8   , KC_9  , KC_0   , KC_BSPC,
+    KC_T   , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,                       KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , QK_LEAD,
+    KC_G   , KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   ,                       KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
+    KC_B   , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   ,                       KC_N   , KC_M   , KC_COMM, KC_DOT, NV_SLSH, KC_ENT ,
+                                                 KC_SPC , KC_LALT,      TT_NUM, KC_SPC
   ),
 
   [LAYER_SYM] = LAYOUT_voyager(
@@ -90,10 +90,10 @@ const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
 
     [LAYER_GAME] = {
         // Left
-        BLUE , GREEN, GREEN, GREEN, GREEN, GREEN,
-        BLUE , CYAN , MAGNT, CYAN , CYAN , CYAN ,
-        BLUE , MAGNT, MAGNT, MAGNT, CYAN , CYAN ,
-        BLUE , CYAN , CYAN , CYAN , CYAN , CYAN ,
+        GREEN, BLUE , GREEN, GREEN, GREEN, GREEN,
+        CYAN , BLUE , CYAN , MAGNT, CYAN , CYAN ,
+        CYAN , BLUE , MAGNT, MAGNT, MAGNT, CYAN ,
+        CYAN , BLUE , CYAN , CYAN , CYAN , CYAN ,
                                     CYAN , MAGNT,
 
         // Right
