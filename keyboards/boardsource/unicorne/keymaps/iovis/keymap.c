6d526ceeff97303f1174b188e1f6ab14aa3f27d0
David Marchante <davidmarchan@gmail.com>
2024-12-17 18:39:44 -0500

    feat(sym): tweak symbols

--- keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/iovis/keymap.c
@@ -54,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
        KC_ESC , KC_GRV , KC_AT  , KC_HASH, KC_DLR , KC_PERC,     KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, MY_NWLN, _______,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,     KC_QUES, KC_LCBR, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO,
+       _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,     KC_DQUO, KC_LCBR, KC_LPRN, KC_RPRN, KC_SCLN, MY_ARRW,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
-       _______, KC_LABK, KC_RABK, KC_BSLS, MY_COLN, MY_CODE,     MY_ARRW, KC_RCBR, KC_LBRC, KC_RBRC, KC_SLSH, KC_ENT ,
+       _______, KC_LABK, KC_RABK, KC_BSLS, MY_COLN, MY_CODE,     KC_QUES, KC_RCBR, KC_LBRC, KC_RBRC, KC_SLSH, KC_ENT ,
     //`--------+--------+--------+--------+--------+--------/   \--------+--------+--------+--------+--------+--------'
                                   _______, _______, MY_FARW,     _______, KC_TILD, KC_0
     //                          `+--------+--------+--------'   `--------+--------+--------+'
--- keyboards/zsa/voyager/keymaps/iovis/keymap.c
+++ keyboards/zsa/voyager/keymaps/iovis/keymap.c
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_SYM] = LAYOUT_voyager(
     _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, KC_BSPC,
     _______, KC_GRV , KC_AT  , KC_HASH, KC_DLR , KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, MY_NWLN, XXXXXXX,
-    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,                       KC_QUES, KC_LCBR, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO,
-    _______, KC_LABK, KC_RABK, KC_BSLS, MY_COLN, MY_CODE,                       MY_ARRW, KC_RCBR, KC_LBRC, KC_RBRC, KC_SLSH, KC_ENT ,
+    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_COLN,                       KC_DQUO, KC_LCBR, KC_LPRN, KC_RPRN, KC_SCLN, MY_ARRW,
+    _______, KC_LABK, KC_RABK, KC_BSLS, MY_COLN, MY_CODE,                       KC_QUES, KC_RCBR, KC_LBRC, KC_RBRC, KC_SLSH, KC_ENT ,
                                                  _______, MY_FARW,     KC_TILD, _______
   ),
 
