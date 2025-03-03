28a8b11b0f077af982f674c6893873da8d10e075
David Marchante <davidmarchan@gmail.com>
2025-03-03 00:02:58 -0500

    refactor(rules.mk): extract common flags
    Taken from https://github.com/getreuer/qmk-keymap/blob/main/keyboards/zsa/voyager/keymaps/getreuer/rules.mk

--- keyboards/zsa/voyager/keymaps/iovis/rules.mk
+++ keyboards/zsa/voyager/keymaps/iovis/rules.mk
@@ -1,20 +1,5 @@
-# Set any rules.mk overrides for your specific keymap here.
-# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-AUDIO_ENABLE = no
-CAPS_WORD_ENABLE = yes
-COMBO_ENABLE = yes
-COMMAND_ENABLE = no
-CONSOLE_ENABLE = no
-DEFERRED_EXEC_ENABLE = yes
-DYNAMIC_MACRO_ENABLE = yes
-KEY_OVERRIDE_ENABLE = no
-LAYER_LOCK_ENABLE = yes
-LEADER_ENABLE = yes
-MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
-OS_DETECTION_ENABLE = yes
-POINTING_DEVICE_ENABLE = no
 RGB_MATRIX_CUSTOM_KB = yes
-SPACE_CADET_ENABLE = no
-SRC += features/custom_shift_keys.c
-TAP_DANCE_ENABLE = no
+
+ROOT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
+include ${ROOT_DIR}../../../../../rules.mk
