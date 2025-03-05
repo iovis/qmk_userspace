caeed472b2f917bb93e86b28b4d183f6029926b5
David Marchante <davidmarchan@gmail.com>
2025-03-05 01:43:21 -0500

    feat(symbols): go back to the m dash life

--- keyboards/beekeeb/piantor_pro/keymaps/iovis/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/iovis/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [LAYER_SYM] = LAYOUT_split_3x6_3(
     //,--------+--------+--------+--------+--------+--------.   ,--------+--------+--------+--------+--------+--------.
-       KC_ESC , KC_GRV , KC_LABK, KC_RABK, KC_DLR , KC_HASH,     KC_PERC, KC_AMPR, KC_ASTR, KC_PIPE, KC_EQL , KC_MINS,
+       KC_ESC , KC_GRV , KC_LABK, KC_RABK, KC_DLR , M_DASH ,     KC_PERC, KC_AMPR, KC_ASTR, KC_PIPE, KC_EQL , KC_MINS,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
        _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_TILD,     KC_DQUO, KC_LCBR, KC_LPRN, KC_RPRN, KC_SCLN, KC_COLN,
     //|--------+--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------+--------|
