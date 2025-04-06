5959ef764c68f42fcbe87f7fe5dd3cb76ed09bc8
David Marchante <davidmarchan@gmail.com>
2025-04-06 13:13:10 -0400

    feat(bnk9): add mouse layer

--- keyboards/binepad/bnk9/keymaps/iovis/keymap.c
+++ keyboards/binepad/bnk9/keymaps/iovis/keymap.c
@@ -5,6 +5,7 @@
 
 enum layers {
     LAYER_BASE = 0,
+    LAYER_MOUSE,
     LAYER_NUM,
 };
 
@@ -16,6 +17,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F   , KC_UP  , KC_ESC ,
         KC_LEFT, KC_DOWN, KC_RGHT,
         KC_M   , MS_BTN4, MS_BTN5
+    ),
+    [LAYER_MOUSE] = LAYOUT(
+        TG(LAYER_MOUSE),
+        MS_BTN4, MS_UP  , MS_BTN5,
+        MS_LEFT, MS_DOWN, MS_RGHT,
+        MS_BTN1, MS_BTN3, MS_BTN2
     ),
     [LAYER_NUM] = LAYOUT(
         TG(LAYER_NUM),
@@ -29,14 +36,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /// Encoder
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [LAYER_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [LAYER_NUM] = {ENCODER_CCW_CW(MS_WHLD, MS_WHLU)},
+    [LAYER_MOUSE] = {ENCODER_CCW_CW(MS_WHLU, MS_WHLD)},
+    [LAYER_NUM] = {ENCODER_CCW_CW(MS_WHLU, MS_WHLD)},
 };
 
 /// Combos (https://docs.qmk.fm/features/combo)
 const uint16_t PROGMEM combo_boot[] = {KC_M, KC_UP, KC_ESC, COMBO_END};
-const uint16_t PROGMEM combo_num[] = {MS_BTN4, MS_BTN5, COMBO_END};
+const uint16_t PROGMEM combo_mouse[] = {MS_BTN4, MS_BTN5, COMBO_END};
+const uint16_t PROGMEM combo_num[] = {KC_F, KC_UP, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_boot, QK_BOOT),
+    COMBO(combo_mouse, TG(LAYER_MOUSE)),
     COMBO(combo_num, TG(LAYER_NUM)),
 };
