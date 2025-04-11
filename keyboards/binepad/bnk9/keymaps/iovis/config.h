96559c787aca87687de84f6cc070994cf6d3cc07
David Marchante <davidmarchan@gmail.com>
2025-04-11 16:28:19 -0400

    feat(mouse): tweak mouse settings

--- keyboards/binepad/bnk9/keymaps/iovis/config.h
+++ keyboards/binepad/bnk9/keymaps/iovis/config.h
@@ -2,5 +2,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #pragma once
 
+// Combo
 #define COMBO_TERM 25
-#define COMBO_SHOULD_TRIGGER
+
+// Mouse
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_BASE_SPEED 1200 // Max speed
