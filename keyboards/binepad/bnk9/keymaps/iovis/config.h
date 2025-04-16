1b6d6e30eae98da76d201c0347bd6b9862b7f12d
David Marchante <davidmarchan@gmail.com>
2025-04-16 15:49:05 -0400

    feat(mouse): change mouse movement to constant

--- keyboards/binepad/bnk9/keymaps/iovis/config.h
+++ keyboards/binepad/bnk9/keymaps/iovis/config.h
@@ -6,8 +6,15 @@
 #define COMBO_TERM 25
 
 // Mouse
-#define MK_KINETIC_SPEED
-#define MOUSEKEY_BASE_SPEED 1200 // Max speed
+#define MK_3_SPEED         // Constant speed
+#define MK_C_OFFSET_1 8    // Cursor offset per movement
+#define MK_C_OFFSET_2 16   // Cursor offset per movement
+#define MK_C_INTERVAL_1 16 // Time between cursor movements
+#define MK_C_INTERVAL_2 16 // Time between cursor movements
+#define MK_W_OFFSET_1 1    // Scroll steps per scroll action
+#define MK_W_OFFSET_2 1    // Scroll steps per scroll action
+#define MK_W_INTERVAL_1 80 // Time between scroll steps
+#define MK_W_INTERVAL_2 60 // Time between scroll steps
 
 // Taps
 #define PERMISSIVE_HOLD
