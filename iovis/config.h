c3e214333e3918dff9c21ecad3e1a6e70fdf8f25
David Marchante <davidmarchan@gmail.com>
2025-04-18 16:07:51 -0400

    feat(mouse): configure MS_ACL0

--- iovis/config.h
+++ iovis/config.h
@@ -22,15 +22,19 @@
 // #define LEADER_TIMEOUT 250
 
 // Mouse
-#define MK_3_SPEED         // Constant speed
-#define MK_C_OFFSET_1 8    // Cursor offset per movement
-#define MK_C_OFFSET_2 16   // Cursor offset per movement
-#define MK_C_INTERVAL_1 16 // Time between cursor movements
-#define MK_C_INTERVAL_2 16 // Time between cursor movements
-#define MK_W_OFFSET_1 1    // Scroll steps per scroll action
-#define MK_W_OFFSET_2 1    // Scroll steps per scroll action
-#define MK_W_INTERVAL_1 80 // Time between scroll steps
-#define MK_W_INTERVAL_2 60 // Time between scroll steps
+#define MK_3_SPEED          // Constant speed
+#define MK_C_OFFSET_0 4     // Cursor offset per movement
+#define MK_C_OFFSET_1 8     // Cursor offset per movement
+#define MK_C_OFFSET_2 16    // Cursor offset per movement
+#define MK_C_INTERVAL_0 16  // Time between cursor movements
+#define MK_C_INTERVAL_1 16  // Time between cursor movements
+#define MK_C_INTERVAL_2 16  // Time between cursor movements
+#define MK_W_OFFSET_0 1     // Scroll steps per scroll action
+#define MK_W_OFFSET_1 1     // Scroll steps per scroll action
+#define MK_W_OFFSET_2 1     // Scroll steps per scroll action
+#define MK_W_INTERVAL_0 360 // Time between scroll steps
+#define MK_W_INTERVAL_1 80  // Time between scroll steps
+#define MK_W_INTERVAL_2 60  // Time between scroll steps
 
 // RGB
 #define RGB_MATRIX_TIMEOUT 300000
