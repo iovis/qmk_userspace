975aefb461e3f5e690bc49a346a4d7572ff6ab85
David Marchante <davidmarchan@gmail.com>
2025-04-20 01:10:48 -0400

    chore(layers): add note about base layers order

--- iovis/layers.h
+++ iovis/layers.h
@@ -1,17 +1,18 @@
 #pragma once
 
 enum layers {
+    // Base layers (keep at top or MO() won't work)
     LAYER_BASE = 0,
     LAYER_LINUX,
     LAYER_GAME,
     LAYER_DWARF,
+
+    LAYER_CALL,
+    LAYER_MOUSE,
+    LAYER_MPV,
     LAYER_NAV,
     LAYER_NUM,
     LAYER_NUMFN,
     LAYER_NUMGM,
-    LAYER_MOUSE,
     LAYER_SYM,
-
-    LAYER_CALL,
-    LAYER_MPV,
 };
