b6036556a0f7bc4554fb695a4981d7257c7e7b8b
David Marchante <davidmarchan@gmail.com>
2025-04-19 22:39:01 -0400

    feat(layers.dwarf_fortress): add a dwarf fortress layer

--- iovis/layers.h
+++ iovis/layers.h
@@ -2,14 +2,16 @@
 
 enum layers {
     LAYER_BASE = 0,
-    LAYER_CALL,
-    LAYER_GAME,
     LAYER_LINUX,
-    LAYER_MOUSE,
-    LAYER_MPV,
+    LAYER_GAME,
+    LAYER_DWARF,
     LAYER_NAV,
     LAYER_NUM,
     LAYER_NUMFN,
     LAYER_NUMGM,
+    LAYER_MOUSE,
     LAYER_SYM,
+
+    LAYER_CALL,
+    LAYER_MPV,
 };
