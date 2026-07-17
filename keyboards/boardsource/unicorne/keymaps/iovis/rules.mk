7e00c25f3cf8abda3a7c977332fa518eea921931
David Marchante <davidmarchan@gmail.com>
2026-07-16 23:33:20 -0400

    feat(unicorne): disable encoder and oled support
    UF2 reduced from 105,472 to 93,696 bytes

--- keyboards/boardsource/unicorne/keymaps/iovis/rules.mk
+++ keyboards/boardsource/unicorne/keymaps/iovis/rules.mk
@@ -1,6 +1,13 @@
 BOOTLOADER = rp2040
+
+ENCODER_ENABLE = no
 RGB_MATRIX_ENABLE = no
 VIA_ENABLE = no
+
+# The keyboard rules add this source even when OLED support is disabled.
+OLED_ENABLE = no
+UNICORNE_SRC := $(filter-out lib/oled.c,$(SRC))
+SRC = $(UNICORNE_SRC)
 
 ROOT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 include ${ROOT_DIR}../../../../../rules.mk
